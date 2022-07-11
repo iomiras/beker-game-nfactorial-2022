2fe9f9a25dda47681676dcb11ca7b615096524b9
shltv <miras.sh03@gmail.com>
2022-07-11 16:11:39 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { elements } from './data';
 
 function App() {
+  const [index, setIndex] = useState(0)
+  const [userValue, setUserValue] = useState('')
+  const [priceInputState, setPriceInputState] = useState('')
+  const [realPriceState, setRealPriceState] = useState('none')
+  const [score, setScore] = useState(0)
+  const [points, setPoints] = useState(0)
+  const element = elements[index]
+
+  const showResults = () => {
+    setPriceInputState('none')
+    setRealPriceState('')
+    let userPrice = parseInt(userValue)
+    let realPrice = parseInt(element.intPrice)
+    let deltaPrice = Math.abs(userPrice - realPrice)
+    if (deltaPrice > 0.85 * realPrice) { setPoints(0); setScore(score + 0) }
+    else {
+      setPoints(Math.floor(1000 - deltaPrice / realPrice * 1000));
+      setScore(score + points)
+    }
+    // if (index < 2) setIndex(index + 1)
+    // else setIndex(0)
+  }
+
+  const handleChange = (event) => {
+    setUserValue(event.target.value)
+  }
+
+  //  style={{ display: "none" }}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&family=Rubik:wght@300&display=swap" rel="stylesheet" />
+
+      <div className='game-wrapper'>
+        <div className='round-points'>
+          Round {index + 1}/3
+        </div>
+
+        <div className='item-name'>
+          {element.name}
+        </div>
+
+        <div className='image-wrapper'>
+          <div className='price-wrapper' style={{ display: realPriceState ? realPriceState : '' }}>
+            <div className='real-price'>
+              <div className='item-price' >{element.price} ₸</div>
+              <div className='item-price'>{element.usdPrice} $</div>
+            </div>
+          </div>
+          <img src={element.photo} alt="auction item" />
+        </div>
+
+        <div className='item-desc'>
+          {element.desc}
+        </div>
+
+        <div className='guess-row' style={{ display: priceInputState ? priceInputState : '' }}>
+          <input className='price-input' placeholder='₸0' type="number" onChange={handleChange} value={userValue} />
+          <button className='submit' onClick={showResults}></button>
+        </div>
+
+        <div className='next-row' style={{ display: realPriceState ? realPriceState : '' }}>
+          <div className='points'>{points} points</div>
+          <button className='next-button'>Next</button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,160 @@
+import Gora from "./shygyn-photos/gora.jpg"
+import Yurta from "./shygyn-photos/yurta.jpg"
+import Turkestan from "./shygyn-photos/turkistan.jpg"
+import Komar from "./shygyn-photos/komar.jpg"
+import Zko from "./shygyn-photos/zko.jpg"
+import Lrt from "./shygyn-photos/lrt.jpg"
+import KeruenSarai from "./shygyn-photos/keruenSarai.jpg"
+import starSky from "./shygyn-photos/starSky.jpg"
+import Article from "./shygyn-photos/article.png"
+import Band from "./shygyn-photos/band.jpg"
+import DaVinci from "./shygyn-photos/daVinci.jpg"
+import Abay from "./shygyn-photos/abay.jpg"
+import Amanat from "./shygyn-photos/amanat.jpg"
+import Mall from "./shygyn-photos/mall.jpeg"
+import Tulen from "./shygyn-photos/tulen.jpg"
+import Posol from "./shygyn-photos/posol.jpg"
+import DeepGorod from "./shygyn-photos/deepgorod.jpg"
+
+const data = [
+    {
+        "photo": Gora,
+        "price": "2 500 000",
+        "intPrice": 2500000,
+        "usdPrice": "5 286",
+        "name": "Фуршет для посольства Казахстана в Бишкеке",
+        "desc": "Фуршет состоял из гор конины и чак-чака, дерева из баурсаков, и сырных лепешок в виде пшеницы"
+    },
+    {
+        "photo": Yurta,
+        "price": "74 636 800",
+        "intPrice": 74636800,
+        "usdPrice": "200 000",
+        "name": "ЮРТА 360 градусов и 3600 декоративных яблок",
+        "desc": "Юрта была установлена в Париже в честь 1000-летия Алматы в 2016 году"
+    },
+    {
+        "photo": Turkestan,
+        "price": "30 900 000",
+        "intPrice": 30900000,
+        "usdPrice": "65 333",
+        "name": "Два фильма, снятых в Туркестана",
+        "desc": 'Первое кино посвящено строительству Туркестана, а второй называется "История - Археология". Но оба фильма не вышли на свет'
+    },
+    {
+        "photo": Komar,
+        "price": "500 000 000",
+        "intPrice": 500000000,
+        "usdPrice": "1 057 165",
+        "name": "Уничтожение комаров в Павлодаре",
+        "desc": 'В Павлодаре была проведена борьба с комарами, но она оказалась абсолютно безрезультатной'
+    },
+    {
+        "photo": Zko,
+        "price": "4 400 000",
+        "intPrice": 4400000,
+        "usdPrice": "9 304",
+        "name": 'Программа "Маслихат"',
+        "desc": 'Депутаты ЗКО будут раз в неделю выходить в эфир. До конца года уральцам покажут депутатов на 333 минуты'
+    },
+    {
+        "photo": Lrt,
+        "price": "851 367 060 000",
+        "intPrice": 851060000,
+        "usdPrice": "1 800 000 000",
+        "name": 'LRT в Астане',
+        "desc": 'No comments.'
+    },
+    {
+        "photo": KeruenSarai,
+        "price": "87 000 000 000",
+        "intPrice": 87000000000,
+        "usdPrice": "183 946 623",
+        "name": 'Керуен-Сарай в Туркестане',
+        "desc": 'Стены «Керуен-сарая» в Туркестане оказались из пенопласта ...'
+    },
+    {
+        "photo": starSky,
+        "price": "38 700 000",
+        "intPrice": 38700000,
+        "usdPrice": "81 825",
+        "name": 'Звездное небо в Актау',
+        "desc": 'В Актау за счет жителей города повесили гирлянду на 105000 лампочек'
+    },
+    {
+        "photo": Article,
+        "price": "275 100",
+        "intPrice": 275100,
+        "usdPrice": "582",
+        "name": 'Статья на заказ акимата в Кызылорде',
+        "desc": 'Статьи состоят из фото акима области на обложке'
+    },
+    {
+        "photo": Band,
+        "price": "19 700 000",
+        "intPrice": 19700000,
+        "usdPrice": "41 652",
+        "name": 'Выступление казахстанской группы в Великобритании',
+        "desc": 'Они выступили на открытии выставки "Золото Великой степи", которая прошла в музее Кембриджа'
+    },
+    {
+        "photo": DaVinci,
+        "price": "72 000",
+        "intPrice": 72000,
+        "usdPrice": "152",
+        "name": 'Книга "Код да Винчи" для Академии госуправления',
+        "desc": 'Рыночная стоимость данной книги 14 раз дешевле'
+    },
+    {
+        "photo": Abay,
+        "price": "145 000 000",
+        "intPrice": 145000000,
+        "usdPrice": "306 578",
+        "name": 'Памятник Абаю в Атырау',
+        "desc": 'Идентичный памятник в Павлодаре стоит 55 млн тенге'
+    },
+    {
+        "photo": Amanat,
+        "price": "6 000 000 000",
+        "intPrice": 6000000000,
+        "usdPrice": "12 685 974",
+        "name": 'Годовое содержание партии Amanat',
+        "desc": 'Amanat is aka Nur Otan'
+    },
+    {
+        "photo": Mall,
+        "price": "472 981 700 000",
+        "intPrice": 472981700000,
+        "usdPrice": "1 000 000 000",
+        "name": 'Ellington mall',
+        "desc": 'Туристический район Ellington mall будет построен в столице'
+    },
+    {
+        "photo": Tulen,
+        "price": "9 000 000",
+        "intPrice": 9000000,
+        "usdPrice": "19 029",
+        "name": 'Фигура тюленя',
+        "desc": 'СМИ писали, что тюленя установили на деньги спонсоров. Но на сайте госзакупок говорится обрантное - деньги выделены из райнонного бюджета'
+    },
+    {
+        "photo": Posol,
+        "price": "5 751 552",
+        "intPrice": 5751552,
+        "usdPrice": "12 160",
+        "name": 'Аренда виллы',
+        "desc": 'Посольство Казахстана в Финлядндии долгие годы арендовало виллу для своих сотрудников.'
+    },
+    {
+        "photo": DeepGorod,
+        "price": "28 000 000 000",
+        "intPrice": 28000000000,
+        "usdPrice": "59 201 212",
+        "name": 'Постройка особняков в столице',
+        "desc": 'Дипломатический городок строят для иностранных посольств и консульств. Эти коттеджи, построенные за наш с вами счет, сдают бесплатно.'
+    }
+]
+
+const questionsNumber = 3
+const shuffled = [...data].sort(() => 0.5 - Math.random());
+export const elements = shuffled.slice(0, questionsNumber)
